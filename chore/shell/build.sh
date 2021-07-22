f26f5a816ca13238b32af761b3e8bf13e3df4ef5
zhangbinbin <zhangbinbin@kuaishou.com>
2021-07-22 12:19:09 +0800

    feat: 开发

--- chore/shell/build.sh
+++ chore/shell/build.sh
@@ -1,7 +1,13 @@
+docker stop cicd
+
+docker rm cicd
+
 yarn --registry=https://registry.npm.taobao.org
 
 yarn
 
 yarn build && echo 'build 完成'
 
-docker build -t zbb/testcicd:v1 .
+docker build -t zbb/testcicd:v1 . && echo '镜像打包完成'
+
+docker run -itd --name cicd -p 8082:80 zbb/testcicd:v1 /bin/bash && echo '上线完成'
